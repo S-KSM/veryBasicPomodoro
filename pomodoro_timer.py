3b099499ad4fc1b5265ac3a2507f44e498697323
Shobeir K. S. Mazinani <shobeir.mazinani@gmail.com>
2022-01-01 22:08:09 -0500

    Add files via upload

---
+++ pomodoro_timer.py
@@ -0,0 +1,29 @@
+from tqdm import tqdm
+from time import sleep
+import argparse
+from pygame import mixer
+
+def timer(total_minutes:int = 25)-> None:
+    """
+    Gets the total_minutes you want to use the pomodoro clock for.
+    """
+
+    for i in tqdm(range(total_minutes*60)):
+        sleep(1)
+
+if __name__ == '__main__':
+    
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-m" ,"--minutes", help="Get the number of minutes for pomodoro",
+                    type=int)
+    args = parser.parse_args()
+    if args.minutes:
+        timer(int(args.minutes))
+    else:
+        timer() # default 25 minutes
+    # play a beep sound
+    mixer.init()
+    sound = mixer.Sound('bell.wav')
+    sound.play()
+    print(f"Congratulations, You did {args.minutes} minutes of focused work!!!")
+    
